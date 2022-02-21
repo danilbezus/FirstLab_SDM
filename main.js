b6473ba669dfbcfe5c340f48ce5e0c551b587fb6
Danil Bezus <danil.bezus.db@gmail.com>
2022-02-21 22:43:40 +0200

    Add function Get_discriminant and Solve It can find the discriminant and get the roots of the expression

--- main.js
+++ main.js
@@ -2,6 +2,16 @@
 
 const fs = require('fs');
 
+function noninteractive(values) {
+  const [a, b, c] = values;
+  if (a === 0) {
+    console.log('a is 0');
+    process.exit(1);
+  }
+  const d = Get_discriminant(a, b, c);
+  Solve(a, b, c, d);
+}
+
 fs.readFile(process.argv[2], 'utf8', function (err, contents) {
   Get_noninteractive_input(contents);
 });
@@ -19,5 +29,23 @@ function Get_noninteractive_input(values) {
     process.exit(1);
   }
 
-  console.log(converted_values_list);
+  noninteractive(converted_values_list);
+}
+
+function Get_discriminant(a, b, c) {
+  return Math.pow(b, 2) - 4 * a * c;
+}
+
+function Solve(a, b, c, d) {
+  console.log(`Equation is: (${a}) x^2 + (${b}) x + (${c}) = 0`);
+  if (d > 0) {
+    const x1 = (-b - Math.sqrt(d)) / (2 * a);
+    const x2 = (-b + Math.sqrt(d)) / (2 * a);
+    console.log('There are 2 roots');
+    console.log(`x1 = ${x1}, x2 = ${x2}`);
+  } else if (d == 0) {
+    let x = -(b / (2 * a));
+    console.log('There are 1 root');
+    console.log(`x = ${x}`);
+  } else console.log('There are 0 roots');
 }
